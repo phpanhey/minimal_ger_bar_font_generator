1cff1c3f674bab268998e7e316284894c2aef926
ppp <philipp.panhey@gmail.com>
2023-12-01 23:51:15 +0100

    finalized colored font generator

---
+++ makefont.sh
@@ -0,0 +1,2 @@
+python3 minimal_ger_bar_font_generator.py;
+python3 addsvg.py svg fonts/minimal_ger_bar_font.otf;
--- minimal_ger_bar_font_generator.py
+++ minimal_ger_bar_font_generator.py
@@ -1,34 +1,53 @@
-import svgwrite
 import os
 
 def main():
     abc = {
-    'A': '#0069b4', 
-    'B': '#FF0000', 
-    'C': '#00FF00', 
-    'D': '#2f4f30', 
-    'E': '#191970', 
-    'F': '#FF00FF', 
-    'G': '#411273', 
-    'H': '#009a93', 
-    'I': '#312783', 
-    'J': '#FF6347', 
-    'K': '#808080', 
-    'L': '#00FA9A', 
-    'M': '#FFA07A', 
-    'N': '#1E90FF', 
-    'O': '#FFD700', 
-    'P': '#511b54', 
-    'Q': '#92580c', 
-    'R': '#21339e', 
-    'S': '#4682B4', 
-    'T': '#40E0D0', 
-    'U': '#0a8b4d', 
-    'V': '#ffed00', 
-    'W': '#d74d55', 
-    'X': '#703034', 
-    'Y': '#f3a600', 
-    'Z': '#9c5f65' ,
+    'A' : 	'#0174df',
+    'B' :	'#f14d4d',
+    'C' :	'#86b404',
+    'D' :	'#31b404',
+    'E' :	'#0b0b3b',
+    'F' :	'#951b81',
+    'G' :	'#662483',
+    'H' :	'#009a93',
+    'I' :	'#312783',
+    'J' :	'#8a181f',
+    'K' :	'#585858',
+    'L' :	'#0f8a09',
+    'M' :	'#ea5b0c',
+    'N' :	'#3558dc',
+    'O' :	'#f1eb42',
+    'P' :	'#511b54',
+    'Q' :	'#92580c',
+    'R' :	'#21339e',
+    'S' :	'#055f84',
+    'T' :	'#07a5b3',
+    'U' :	'#0a8b4d',
+    'V' :	'#cc89d4',
+    'W' :	'#d74d55',
+    'X' :	'#703034',
+    'Y' :	'#f3a600',
+    'Z' :	'#9c5f65'    
+    }
+
+    other_chars = { 
+        'germandbls': '#a9f5a9', #'ß'
+        'zero': '#2ECCFA', #0
+        'one': '#F5A9E1', # '1'
+        'two': '#D7DF01', # '2'
+        'three': '#D8F781', # '3'
+        'four': '#A9A9F5', # '4'
+        'five': '#F78181', # '5'
+        'six': '#819FF7', # '6'
+        'seven': '#A9F5D0', # '7'
+        'eight': '#BDBDBD', # '8'
+        'nine': '#F3E2A9',  # '9'
+    }
+
+    umlauts = { 
+        'adieresis' :	'#0b4c5f', # 'ä'
+        'odieresis' :	'#f5a9a9', # 'ö'        
+        'udieresis' :	'#a9bcf5', # 'ü'        
     }
 
     create_directories()
@@ -36,22 +55,49 @@ def main():
     for char, hash in abc.items():
         create_uppercase_lowercase_svg(char,hash)
 
+    for char, hash in other_chars.items():
+        create_svg(char,hash)
+
+    for char, hash in umlauts.items():
+        create_uppercase_lowercase_umlauts_svg(char,hash)
+
 def create_directories():
-    upper_directory = 'upper'
-    lower_directory = 'lower'
+    svg_directory = 'svg'
+    upper_directory = f'{svg_directory}/upper_characters'
+    lower_directory = f'{svg_directory}/lower_characters'
+    other_chars_directory = f'{svg_directory}/other_chars'
+    if not os.path.exists(svg_directory):
+        os.makedirs(svg_directory)  
     if not os.path.exists(upper_directory):
         os.makedirs(upper_directory)  
     if not os.path.exists(lower_directory):
         os.makedirs(lower_directory)  
+    if not os.path.exists(other_chars_directory):
+        os.makedirs(other_chars_directory)  
 
 def create_uppercase_lowercase_svg(character,color):
     upper_svg = f"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -1000 1000 1000'>  <path fill='{color}' d='M50 92V-692H241V92Z'/> </svg>"
     lower_svg = f"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -1000 1000 1000'>  <path fill='{color}' d='M50 86V-479H242V86Z'/> </svg>"
     
-    with open(f"upper/{character.upper()}.svg", "w") as file:    
+    with open(f"svg/upper_characters/{character.upper()}.svg", "w") as file:    
         file.write(upper_svg)
 
-    with open(f"lower/{character.lower()}.svg", "w") as file:    
+    with open(f"svg/lower_characters/{character.lower()}.svg", "w") as file:    
+        file.write(lower_svg)
+
+def create_svg(character,color):
+    svg = f"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -1000 1000 1000'>  <path fill='{color}' d='M50 92V-692H241V92Z'/> </svg>"
+    with open(f"svg/other_chars/{character}.svg", "w") as file:    
+        file.write(svg)    
+
+def create_uppercase_lowercase_umlauts_svg(character,color):
+    upper_svg = f"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -1000 1000 1000'>  <path fill='{color}' d='M50 92V-692H241V92Z'/> </svg>"
+    lower_svg = f"<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -1000 1000 1000'>  <path fill='{color}' d='M50 86V-479H242V86Z'/> </svg>"
+    
+    with open(f"svg/upper_characters/{character.capitalize()}.svg", "w") as file:    
+        file.write(upper_svg)
+
+    with open(f"svg/lower_characters/{character.lower()}.svg", "w") as file:    
         file.write(lower_svg)
 
 if __name__ == '__main__':
